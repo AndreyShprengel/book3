b74c675f24af360d7b158b218fc89fa6cd042084
Andrey Shprengel <andrey.shprengel@colorado.edu>
2015-11-12 23:36:58 -0700

    pulling zayo

---
+++ hackathons/bitcoin/bot.js
@@ -0,0 +1,8 @@
+//
+// TODO: implement the logic to decide whether or not to make a trade
+//
+var dict = [{word: "game, count: 0, effect: 0}]
+function decideWhetherOrNotToTrade(tweet){
+  //console.log("tweet", tweet.tweet)
+  return Math.random() < 0.1
+}
